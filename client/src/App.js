e6f061ca656102a2a24aff2f128ce0253c7ef0c4
Gabriel Kassen <gkassen16@gmail.com>
2022-02-21 17:41:50 -0300

    Initial commit to our app

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    response: {}
+  };
+  
+  componentDidMount() {
+    axios.get('/api/v1/say-something').then((res) => {
+      const response = res.data;
+      this.setState({response});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from the frontend!</h1>
+        <h1>{this.state.response.body}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
